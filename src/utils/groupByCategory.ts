9f045fb9acfd3716843e8d9611eb964fc12f5a30
gjealfaro <gjealfaro@gmail.com>
2026-07-26 18:45:10 +0800

    Green: implement grouping for single resource

--- src/utils/groupByCategory.ts
+++ src/utils/groupByCategory.ts
@@ -1,5 +1,11 @@
 import { Resource } from '../types/Resource';
 
-export function groupByCategory(resources: Resource[]) {
-  return {};
+export function groupByCategory(resources: Resource[]): Record<string, Resource[]> {
+  return resources.reduce((acc, resource) => {
+    if (!acc[resource.category]) {
+      acc[resource.category] = [];
+    }
+    acc[resource.category].push(resource);
+    return acc;
+  }, {} as Record<string, Resource[]>);
 }
