f32effeb2f9fd9fa43ec6a030da152eb29ccad22
gjealfaro <gjealfaro@gmail.com>
2026-07-26 19:38:47 +0800

    Green: conditionally render img in ResourceCard only when thumbnail exists

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -3,11 +3,12 @@ import { Resource } from "../types/Resource";
 interface Props {
   resource: Resource;
 }
-
 export function ResourceCard({ resource }: Props) {
   return (
     <div>
-      <img src={resource.thumbnail} alt={resource.title} />
+      {resource.thumbnail && (
+        <img src={resource.thumbnail} alt={resource.title} />
+      )}
       <p>{resource.title}</p>
       <ul>
         {resource.tags.map((tag) => (
