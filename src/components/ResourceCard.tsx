c0f8ecf511ffb3e291591742a9092122c7e82ffc
gjealfaro <gjealfaro@gmail.com>
2026-07-26 19:26:26 +0800

    Green: render tags in ResourceCard

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -8,7 +8,12 @@ export function ResourceCard({ resource }: Props) {
   return (
     <div>
       <img src={resource.thumbnail} alt={resource.title} />
-      {resource.title}
+      <p>{resource.title}</p>
+      <ul>
+        {resource.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     </div>
   );
 }
