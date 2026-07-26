a016eda7bc5137b750d7058690219bb52549fb76
gjealfaro <gjealfaro@gmail.com>
2026-07-26 18:19:43 +0800

    Red: added failing test for groupByCategory with an empty array

---
+++ src/types/Resource.ts
@@ -0,0 +1,10 @@
+export interface Resource {
+  id: string;
+  category: string;
+  title: string;
+  thumbnail: string;
+  tags: string[];
+  duration: number;
+  description: string;
+  date_uploaded: string;
+}
